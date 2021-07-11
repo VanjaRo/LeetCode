79a962ca78be9a39e2e67af392443f80a5ba6ec7
VanjaRo <vanjaro43@gmail.com>
2021-07-11 19:14:36 +0300

    Solved 88 on LeetCode.

--- tasks/88.go
+++ tasks/88.go
@@ -1,23 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	merge(nums1, 3, []int{4, 5, 6}, 3)
+	fmt.Print(nums1)
 }
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	var tmp int
-	var j int
-	for i := range nums1 {
-		if i > m-1 {
-			if tmp < nums2[j] {
-				nums1[i] = tmp
-			}
-			nums1[i] = nums2[j]
-			j++
+	var i, j = m - 1, n - 1
+	for l := len(nums1) - 1; l >= 0; l-- {
+		if j < 0 || i >= 0 && nums1[i] >= nums2[j] {
+			nums1[l] = nums1[i]
+			i--
+		} else {
+			nums1[l] = nums2[j]
+			j--
 		}
-		if nums1[i] > nums2[j] {
-			tmp
-		}
-
 	}
 }
